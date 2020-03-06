0720b72e25684bc87698f2e0655ff11cd5f0615f
root <root@bastion.0aa8.example.opentlc.com>
2020-03-06 17:41:20 +0000

    trying to get fronend toansible ping

--- test.sh
+++ test.sh
@@ -1,2 +1,2 @@
 OSP_GUID=c0af
-ansible-playbook site-setup-workstation.yml -e OSP_GUID=${OSP_GUID} --private-key=/root/.ssh/id_rsa -u bradley-csdef.com --ask-vault-pass
+ansible-playbook site-setup-workstation.yml -e OSP_GUID=${OSP_GUID} --private-key=/root/.ssh/openstack.pem -u bradley-csdef.com --ask-vault-pass
